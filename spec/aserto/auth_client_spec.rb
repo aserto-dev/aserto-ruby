1ffec1dd7474c8b7f3cf2361f756caec9aabc27b
Gimmy <gheorghe.popescu@aserto.com>
2022-07-26 10:01:06 +0300

    tests for default decision

--- spec/aserto/auth_client_spec.rb
+++ spec/aserto/auth_client_spec.rb
@@ -49,6 +49,58 @@ describe Aserto::AuthClient do
         expect(client.is).to be_falsey
       end
     end
+
+    context "when chaning default decision" do
+      let(:initial_decision) { Aserto.config.decision }
+
+      before do
+        Aserto.configure do |config|
+          config.decision = "visible"
+        end
+      end
+
+      after do
+        Aserto.configure do |config|
+          config.decision = initial_decision
+        end
+      end
+
+      context "when visible" do
+        before do
+          GrpcMock.stub_request("/aserto.authorizer.authorizer.v1.Authorizer/Is").to_return do
+            Aserto::Authorizer::Authorizer::V1::IsResponse.new(
+              { decisions: [
+                { decision: "allowed", is: false },
+                { decision: "visible", is: true },
+                { decision: "enabled", is: false }
+              ] }
+            )
+          end
+        end
+
+        it "returns true" do
+          expect(client.is).to be_truthy
+        end
+      end
+
+      context "when not visible" do
+        before do
+          GrpcMock.stub_request("/aserto.authorizer.authorizer.v1.Authorizer/Is").to_return do
+            Aserto::Authorizer::Authorizer::V1::IsResponse.new(
+              { decisions: [
+                { decision: "allowed", is: true },
+                { decision: "visible", is: false },
+                { decision: "enabled", is: true }
+              ] }
+            )
+          end
+        end
+
+        it "returns true" do
+          expect(client.is).to be_falsey
+        end
+      end
+    end
   end
 
   describe ".allowed?" do
