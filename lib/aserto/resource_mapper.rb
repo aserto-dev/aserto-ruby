d6073526ad55262e2ed83e70cad74039628f515c
Gimmy <gheorghe.popescu@aserto.com>
2022-08-01 10:51:49 +0300

    add resource context mapping

--- lib/aserto/resource_mapper.rb
+++ lib/aserto/resource_mapper.rb
@@ -3,7 +3,27 @@
 module Aserto
   module ResourceMapper
     class << self
-      def execute(_request)
+      def execute(request)
+        if defined? ::Rails
+          params = request.params
+          return {} unless params.is_a?(Hash) && !params.empty?
+
+          require_relative "rails/utils"
+
+          route = Aserto::Rails::Utils.route(request)
+          path = route[:path] if route
+          return {} unless path
+
+          fields = path.split("/")
+                       .select { |part| part.starts_with?(":") }
+                       .map { |field| field.sub(":", "") }
+          return {} if fields.empty?
+
+          require "google/protobuf/well_known_types"
+          return Google::Protobuf::Struct.from_hash(fields.to_h { |field| [field, params[field]] })
+
+        end
+
         {}
       end
     end
